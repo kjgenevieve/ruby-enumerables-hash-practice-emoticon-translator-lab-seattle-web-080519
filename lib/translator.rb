2bf5129045e8c39120ee8d4f550cc8b86a4cc00b
Genevieve McAllister <kj.genevieve@gmail.com>
2019-08-05 00:59:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,16 @@
-# require modules here
-
-def load_library
-  # code goes here
+require 'yaml'
+  
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  
+  hash = {
+    get_meaning: {},
+    get_emoticon: {}
+  }
 end
+
+
+
 
 def get_japanese_emoticon
   # code goes here
