6b364a6f640f1a913bd1f9ce02b955e9f8fa8f7b
Genevieve McAllister <kj.genevieve@gmail.com>
2019-08-05 02:03:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,9 @@ def load_library(file_path)
   }
 
   emoticons.each do |english_word, emoticon_ary|
-    get_meaning_dict = emoticon_translations[get_meaning]
-    get_meaning_dict[emoticon_ary[1]] = [english_word]
+    print('hi')
+    # get_meaning_dict = emoticon_translations[get_meaning]
+    # get_meaning_dict[emoticon_ary[1]] = [english_word]
   end
         
 #     get_emoticon_dict = emoticon_translations[get_emoticon]
